cadf344ff0b5196f97384f4441c014c0a482baba
Reda Lemeden <git@redalemeden.com>
2025-07-12 15:58:29 +0200

    Handle nil local time in timestamp function gracefully

--- Sources/FileLogger/FileLogger.swift
+++ Sources/FileLogger/FileLogger.swift
@@ -91,7 +91,9 @@ public struct FileLogger: LogHandler {
   private func timestamp() -> String {
     var buffer = [Int8](repeating: 0, count: 255)
     var timestamp = time(nil)
-    let localTime = localtime(&timestamp)
+    guard let localTime = localtime(&timestamp) else {
+      return ""
+    }
     strftime(&buffer, buffer.count, "%Y-%m-%dT%H:%M:%S%z", localTime)
     return buffer.withUnsafeBufferPointer {
       $0.withMemoryRebound(to: CChar.self) {
